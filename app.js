3f81c934eba5e2dd6feb1c2fc55b78de9b42a766
Billy <Lrdweaver@gmail.com>
2022-12-06 05:41:53 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+function book(title, author, bookLength, finished) {
+    this.title = title;
+    this.author = author;
+    this.bookLength = bookLength;
+    this.finished = finished
+};
+
+function addBookToLibrary() {
+    myLibrary.push()
+};
+
+console.log(myLibrary)
