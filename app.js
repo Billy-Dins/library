4fe8416bee8b44d153617ecd1ecdf7c930fd2bd8
Billy <Lrdweaver@gmail.com>
2022-12-06 19:48:09 -0500

    Update list with JS

--- app.js
+++ app.js
@@ -2,17 +2,38 @@ let myLibrary = [];
 
 let bookList = document.querySelector('#bookListContent')
 
-function book() {
+function Book() {
     this.title = document.querySelector('#bookTitle').value;
     this.author = document.querySelector('#bookAuthor').value;
     this.bookLength = document.querySelector('#bookPages').value;
-    this.finished = document.querySelector('#finished').value
 };
 
-function addBookToLibrary() {
-    let newBook = new book()
-    console.log(newBook)
-    myLibrary.push(newBook)
-    console.log(myLibrary)
-    bookList.textContent = myLibrary[0].title
-}
+let addBook = document.getElementById('addBook');
+addBook.addEventListener('click', addBookToLibrary);
+
+function addBookToLibrary(e) {
+    e.preventDefault()
+    let books = new Book()
+    myLibrary.push(books)
+
+    let myBooks = document.createElement('div')
+    myBooks.setAttribute('style', 'border: 1px solid black; margin-bottom: 5px; background-color: rgb(0, 0, 0, .2);')
+
+    let myTitle = document.createElement('li')
+    myTitle.setAttribute('style', 'margin: 0px;')
+    myTitle.textContent = myLibrary.at(-1).title
+    myBooks.appendChild(myTitle)
+
+    let myAuthor = document.createElement('li')
+    myAuthor.setAttribute('style', 'margin: 0px;')
+    myAuthor.textContent = myLibrary.at(-1).author
+    myBooks.appendChild(myAuthor)
+
+    let myPages = document.createElement('li')
+    myPages.setAttribute('style', 'margin: 0px;')
+    myPages.textContent = myLibrary.at(-1).bookLength
+    myBooks.appendChild(myPages)
+
+
+    bookList.appendChild(myBooks)
+};
