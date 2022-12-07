c20e15c12abc1ebf12446ad1e1683b13eeba7eb9
Billy <Lrdweaver@gmail.com>
2022-12-07 06:33:47 -0500

    added complete and delete buttons

---
+++ README.md
@@ -0,0 +1,3 @@
+To do:
+- Add a read/completed option
+- Add a delete book option (array.slice()) should probably work
--- app.js
+++ app.js
@@ -17,23 +17,43 @@ function addBookToLibrary(e) {
     myLibrary.push(books)
 
     let myBooks = document.createElement('div')
-    myBooks.setAttribute('style', 'border: 1px solid black; margin-bottom: 5px; background-color: rgb(0, 0, 0, .2);')
+    myBooks.setAttribute('id', 'myBooks')
 
     let myTitle = document.createElement('li')
-    myTitle.setAttribute('style', 'margin: 0px;')
+    myTitle.setAttribute('id', 'bookTitle')
     myTitle.textContent = myLibrary.at(-1).title
     myBooks.appendChild(myTitle)
 
     let myAuthor = document.createElement('li')
-    myAuthor.setAttribute('style', 'margin: 0px;')
+    myAuthor.setAttribute('id', 'bookAuthor')
     myAuthor.textContent = myLibrary.at(-1).author
     myBooks.appendChild(myAuthor)
 
     let myPages = document.createElement('li')
-    myPages.setAttribute('style', 'margin: 0px;')
+    myPages.setAttribute('id', 'bookLength')
     myPages.textContent = myLibrary.at(-1).bookLength
     myBooks.appendChild(myPages)
 
+    let deleteBook = document.createElement('button')
+    deleteBook.setAttribute('id', 'deleteBtn')
+    deleteBook.setAttribute('onclick', 'deleteThisBook(event)')
+    deleteBook.textContent = 'delete'
+    myBooks.appendChild(deleteBook)
+
+    let toggleRead = document.createElement('button');
+    toggleRead.setAttribute('id', 'readBtn');
+    toggleRead.setAttribute('onclick', 'toggleReadEvent(event)')
+    toggleRead.textContent = 'complete'
+    myBooks.appendChild(toggleRead)
 
     bookList.appendChild(myBooks)
 };
+
+function deleteThisBook(event) {
+    event.preventDefault()
+    console.log('hello world')
+}
+
+function toggleReadEvent(event) {
+    event.preventDefault();
+}
