2400219195cdd5b4b7eb29616d6dcf5aae28070e
Billy <Lrdweaver@gmail.com>
2022-12-19 10:27:43 -0500

    corrected let to const(s)

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-let myLibrary = [];
+const myLibrary = [];
 
 const addBookBtn = document.querySelector('#addBookBtn')
-const addBook = document.querySelector('#addBookBtn')
 const libraryShelf = document.querySelector('#bookShelf')
-const deleteBtn = document.querySelector('deleteBtn')
 
 /* book constructor */
-function createBook() {
-    this.title = document.querySelector('#bookTitle').value;
-    this.author = document.querySelector('#bookAuthor').value;
-    this.pages = document.querySelector('#bookPages').value;
-    this.read = document.querySelector('#readBtn').checked
+function createBook(title, author, pages, read) {
+    return {title, author, pages, read}
 };
 /* takes current form data, compiles into an object using constructor,
 then pushes to the myLibrary array before re-rendering the array
 and clearing all input fields */
 addBookBtn.addEventListener('click', () => {
-    let book = new createBook()
+    const title = document.querySelector('#bookTitle').value;
+    const author = document.querySelector('#bookAuthor').value;
+    const pages = document.querySelector('#bookPages').value;
+    const read = document.querySelector('#readBtn').checked
+    const book = createBook(title, author, pages, read)
     myLibrary.push(book)
     libraryShelf.textContent = ''
     renderBooks()
@@ -28,7 +27,7 @@ addBookBtn.addEventListener('click', () => {
 /* toggles the selected book value (read) between true & false,
 clears the display then re-renders the array. */
 function toggleRead(book) {
-    if (book.read == true) {
+    if (book.read === true) {
         book.read = false;
     } else {
         book.read = true;
@@ -54,25 +53,25 @@ function createBookElements(el, content, className) {
 /* Creates a new div, then runs the createBookElements function 
 to create a new book object then appends the book to the div */
 function createNewBook(book, index) {
-    let newBook = document.createElement('div');
+    const newBook = document.createElement('div');
     newBook.setAttribute('class', 'book');
     newBook.setAttribute('id', index);
     newBook.appendChild(createBookElements('h5', `Title: ${book.title}`, 'bookTitle'))
     newBook.appendChild(createBookElements('h5', `Author: ${book.author}`, 'bookAuthor'))
     newBook.appendChild(createBookElements('h5', `Pages: ${book.pages}`, 'bookPages'))
-    let deleteBtn = newBook.appendChild(createBookElements('button', 'x', 'deleteBtn'))
+    const deleteBtn = newBook.appendChild(createBookElements('button', 'x', 'deleteBtn'))
     deleteBtn.addEventListener('click', (e) => {
         e.preventDefault()
         deleteBook(index)
     })
-    if (book.read == true) {
-        let readBtn = newBook.appendChild(createBookElements('button', 'read', 'complete'))
+    if (book.read === true) {
+        const readBtn = newBook.appendChild(createBookElements('button', 'read', 'complete'))
         readBtn.addEventListener('click', (e) => {
             e.preventDefault()
             toggleRead(book, index)
         })
     } else {
-        let readBtn = newBook.appendChild(createBookElements('button', 'read', 'notComplete'))
+        const readBtn = newBook.appendChild(createBookElements('button', 'read', 'notComplete'))
         readBtn.addEventListener('click', (e) => {
         e.preventDefault()
         toggleRead(book, index)
@@ -82,9 +81,10 @@ function createNewBook(book, index) {
 }
 /* Renders the myLibrary, working array onto the webpage by
 creating an individual book object for every item in the array. */
-function renderBooks() {
-    myLibrary.map(function (book,index) {
+const renderBooks = () => {
+    myLibrary.map((book,index) => {
         createNewBook(book,index)
+        return book
     });
 }
 
